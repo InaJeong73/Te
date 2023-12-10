d8bf21dba12347874e3444c760c66fd8dddbf675
Muzi-HS <76544195+Muzi-HS@users.noreply.github.com>
2023-12-10 22:56:14 +0900

    Update authController.js
    Erase Groupby

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -23,30 +23,6 @@ const signUp = async (req, res) => {
             region,
         });
 
-        await db.collection('usersByMajor').doc(uid).set({
-            uid,
-            email,
-            major,
-            grade,
-            region,
-        });
-
-        await db.collection('usersByGrade').doc(uid).set({
-            uid,
-            email,
-            major,
-            grade,
-            region,
-        });
-
-        await db.collection('usersByRegion').doc(uid).set({
-            uid,
-            email,
-            major,
-            grade,
-            region,
-        });
-
         res.status(201).json({ uid: userRecord.uid, message: "회원가입 성공" });
     } catch (error) {
         res.status(500).json({ error: error.message });
@@ -112,29 +88,6 @@ const createProfile = async (req, res) => {
     }
 };
 
-// const createProfile = async (req, res) => {
-//     const { uid, name, birth, phoneNumber, university, experience, major, grade, region } = req.body;
-
-//     try {
-//         await db.collection('users').doc(uid).update({
-//             uid,
-//             name,
-//             birth,
-//             phoneNumber,
-//             university,
-//             experience,
-//             major,
-//             grade,
-//             region,
-//             portfolio,
-//         });
-
-//         res.status(200).json({ message: "프로필 생성 완료" });
-//     } catch (error) {
-//         res.status(500).json({ error: error.message });
-//     }
-// };
-
 const editProfile = async (req, res) => {
     const { uid, name, birth, phoneNumber, university, experience, major, grade, region } = req.body;
 
@@ -175,24 +128,6 @@ const getProfile = async (req, res) => {
         res.status(200).json({ ...profileData, notificationEnabled: profileData.notificationEnabled || false });
     } catch (error) {
         console.error(`Error getting profile: ${error.message}`);
-        res.status(500).json({ error: error.message });
-    }
-};
-
-const getProfileByCategory = async (req, res) => {
-    const { category, value } = req.params;
-
-    try {
-        const querySnapshot = await db.collection(`usersBy${category}`).where(category, '==', value).get();
-
-        const profiles = [];
-        querySnapshot.forEach((doc) => {
-            profiles.push(doc.data());
-        });
-
-        res.status(200).json(profiles);
-    } catch (error) {
-        console.error(`Error getting profiles: ${error.message}`);
         res.status(500).json({ error: error.message });
     }
 };
