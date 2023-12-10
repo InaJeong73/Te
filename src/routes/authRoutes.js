32c554037587a61d5032c41941172eeaca0df8aa
Muzi-HS <76544195+Muzi-HS@users.noreply.github.com>
2023-12-10 16:25:38 +0900

    Update authRoutes.js
    add getAllusers router

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { signUp, login, createProfile, editProfile, getProfile, getProfileByCategory } = require('../controllers/authController');
+const { signUp, login, createProfile, editProfile, getProfile, getProfileByCategory, getAllUsers } = require('../controllers/authController');
 
 router.post('/signup', signUp);
 router.post('/login', login);
@@ -8,5 +8,6 @@ router.post('/createprofile', createProfile);
 router.post('/editprofile', editProfile);
 router.get('/getProfile/:uid', getProfile);
 router.get('/getProfileByCategory/:category/:value', getProfileByCategory);
+router.get('/getAllUsers', getAllUsers);
 
 module.exports = router;    
