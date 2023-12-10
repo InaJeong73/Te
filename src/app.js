b7e68ba0c8ec7d5777c7bfbd8b88b1132824f070
Muzi-HS <76544195+Muzi-HS@users.noreply.github.com>
2023-12-10 15:46:00 +0900

    Update app.js
    add chatRoutes

--- src/app.js
+++ src/app.js
@@ -1,18 +1,17 @@
-// app.js
-const express = require('express');
 require('./config/firebaseConfig');
-
+const express = require('express');
 const authRoutes = require('./routes/authRoutes');
 const postRoutes = require('./routes/postRoutes');
 const applicationRoutes = require('./routes/applicationRoutes');
+const chatRoutes = require('./routes/chatRoutes');
 
 const app = express();
-
 
 app.use(express.json());
 app.use('/api', authRoutes);
 app.use('/api/post', postRoutes);
 app.use('/api/apply', applicationRoutes);
+app.use('/api/chat', chatRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
